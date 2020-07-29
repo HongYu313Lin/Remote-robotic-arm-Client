d3958e2148b776642c5ebb1d2848f8f1c629d127
Hong-Yu,Lin <4a12c062@stust.edu.tw>
2020-07-29 09:02:40 +0800

    加入抬頭

--- TCPIPClient/Program.cs
+++ TCPIPClient/Program.cs
@@ -5,11 +5,16 @@ using System.Windows.Forms;
 
 namespace TCPIPClient
 {
+    /// <summary>
+    /// Ph.D. Program in Engineering Science and Technology, College of Engineering
+    /// National Kaohsiung University of Science and Technology
+    /// 作者:林紘宇 Hong-Yu,Lin
+    /// 正在國立高雄科技大學工程科技研究所攻讀博士學位
+    /// </summary>
     static class Program
     {
         /// <summary>
-        /// 作者:林紘宇 Hong-Yu,Lin
-        /// 正在國立高雄科技大學工程科技研究所攻讀博士學位
+        /// 應用程式的主要進入點。
         /// </summary>
         [STAThread]
         static void Main()
